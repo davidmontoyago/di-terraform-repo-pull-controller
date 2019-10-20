e83fe78a0789aaaf5efac66c3c6039435f0fd9b6
David Montoya <davidmontoyago@gmail.com>
2019-10-20 14:45:14 -0500

    clone repo & checkout contents

--- Makefile
+++ Makefile
@@ -18,6 +18,7 @@ test:
 
 clean:
 	$(GOCLEAN)
+	$(GOCLEAN) ./cmd/...
 	make delete
 
 fmt:
--- cmd/terraform-runner/setup-terraform-repo/main.go
+++ cmd/terraform-runner/setup-terraform-repo/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"bytes"
+	"log"
 	"os"
+	"os/exec"
 
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
 	"k8s.io/klog"
 )
 
 var ()
 
 func main() {
-
 	klog.Infof("Starting terraform-runner...")
 	repoName := os.Getenv("REPO_NAME")
 	klog.Infof("REPO_NAME=%s", repoName)
@@ -20,5 +24,41 @@ func main() {
 	gitSHA := os.Getenv("GIT_SHA")
 	klog.Infof("GIT_SHA=%s", gitSHA)
 
-	// TODO clone repo & plan/apply
+	GitCheckout(repoUrl, gitSHA)
+	ListRepoContents()
+}
+
+func GitCheckout(repoUrl string, gitSHA string) {
+	repo, err := git.PlainClone("/workspace", false, &git.CloneOptions{
+		URL: repoUrl,
+	})
+	TerminateIfError(err, "Failed to clone repo: %v")
+	klog.Infof("Completed cloning repo %s.", repoUrl)
+
+	worktree, err := repo.Worktree()
+	TerminateIfError(err, "Failed to fetch repo work tree: %v")
+
+	err = worktree.Checkout(&git.CheckoutOptions{
+		Hash: plumbing.NewHash(gitSHA),
+	})
+	TerminateIfError(err, "Failed to checkout revision: %v")
+	klog.Infof("Completed repo checkout to %s.", gitSHA)
+}
+
+func ListRepoContents() {
+	cmd := exec.Command("ls", "-al", "/workspace")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	klog.Infof("Listing repo contents... \n%s", out.String())
+}
+
+func TerminateIfError(err error, format string) {
+	if err != nil {
+		klog.Errorf(format, err)
+		os.Exit(1)
+	}
 }
